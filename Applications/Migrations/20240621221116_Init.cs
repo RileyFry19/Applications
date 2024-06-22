d175834d72cb9faffcafc66b3378476358ab7dab
RileyFry19 <rileyfry140@gmail.com>
2024-06-21 20:33:32 -0500

    Initial Commit

---
+++ Applications/Migrations/20240621221116_Init.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Applications.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "apps",
+                columns: table => new
+                {
+                    appId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    company = table.Column<string>(type: "TEXT", nullable: false),
+                    position = table.Column<string>(type: "TEXT", nullable: false),
+                    location = table.Column<string>(type: "TEXT", nullable: false),
+                    date = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    likelyhood = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_apps", x => x.appId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "apps");
+        }
+    }
+}
---
+++ Applications/Models/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Applications.Models;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions options) : base(options)
+    {
+        
+    }
+    
+    public DbSet<Apps> apps { get; set; }
+}
